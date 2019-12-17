bcf952923d9463f2939c6b42864bd1f8aeab0378
Kaio Rosa <kaiosrosa@gmail.com>
2019-12-17 16:34:24 -0300

    add boilerplate code insta app

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+const bodyParser = require('body-parser');
+
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(process.cwd() + '/views/home.html');
+});
+
+app.listen(port, () => console.log(`Conta Web listening on port ${port}!`));
