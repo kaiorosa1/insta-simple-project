bd332606c1b5394e63de5f883106dcacec0028cf
Kaio Rosa <kaiosrosa@gmail.com>
2019-12-17 17:03:59 -0300

    change to ejs

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use('/public', express.static(process.cwd() + '/public'));
-
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.ejs');
 });
 
-app.listen(port, () => console.log(`Conta Web listening on port ${port}!`));
+app.listen(port, () => console.log(`Insta API app listening on port ${port}!`));
